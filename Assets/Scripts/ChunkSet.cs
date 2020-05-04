a8828e31314018600764414907d86ec1811450c7
Joeoc2001 <joeoc2625@gmail.com>
2020-05-04 01:26:45 +0100

    Added support for a priority when updating chunks

--- Assets/Scripts/ChunkSet.cs
+++ Assets/Scripts/ChunkSet.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 
 public class ChunkSet : MonoBehaviour
 {
+    private class ChunkPriority : IComparable<ChunkPriority>
+    {
+        private readonly double lastUpdatedTime;
+
+        public ChunkPriority(double lastUpdatedTime)
+        {
+            this.lastUpdatedTime = lastUpdatedTime;
+        }
+
+        public int CompareTo(ChunkPriority other)
+        {
+            return lastUpdatedTime.CompareTo(other.lastUpdatedTime);
+        }
+    }
+
     public Chunk baseChunk;
 
-    [Range(1, 50)]
+    private int activeViewDistance;
+
+    [Range(1, 100)]
+    public int updatesPerTick = 10;
+
+    [Range(1, 10)]
     public int viewDistance;
 
     [Range(0, 50)]
@@ -18,10 +38,13 @@ public class ChunkSet : MonoBehaviour
 
     private Dictionary<Int3, Chunk> chunks;
 
+    private Priority_Queue.SimplePriorityQueue<Chunk, ChunkPriority> updatePriority;
+
     // Start is called before the first frame update
     void Start()
     {
         chunks = new Dictionary<Int3, Chunk>();
+        updatePriority = new Priority_Queue.SimplePriorityQueue<Chunk, ChunkPriority>();
     }
 
     bool ShouldApproximateNormals(double dist)
@@ -36,37 +59,69 @@ public class ChunkSet : MonoBehaviour
             );
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RegenerateChunks(int dist)
     {
-        Int3 baseIndex = new Int3(transform.position / baseChunk.size);
-
-        for (int x = -viewDistance; x < viewDistance; x++)
+        if (dist == activeViewDistance)
         {
-            for (int y = -viewDistance; y < viewDistance; y++)
+            return;
+        }
+
+        Dictionary<Int3, Chunk> newChunks = new Dictionary<Int3, Chunk>();
+
+        Int3 baseIndex = new Int3(transform.position / baseChunk.size);
+        for (int x = -dist; x < dist; x++)
+        {
+            for (int y = -dist; y < dist; y++)
             {
-                for (int z = -viewDistance; z < viewDistance; z++)
+                for (int z = -dist; z < dist; z++)
                 {
                     Int3 index = baseIndex + new Int3(x, y, z);
 
-                    double dist = Math.Sqrt(x * x + y * y + z * z);
-
-                    Chunk chunk;
-                    if (!chunks.TryGetValue(index, out chunk))
+                    if (chunks.TryGetValue(index, out Chunk chunk))
+                    {
+                        chunks.Remove(index);
+                    }
+                    else
                     {
                         Vector3 offset = index * baseChunk.size;
                         chunk = Instantiate<Chunk>(baseChunk, offset, Quaternion.identity, transform);
 
-                        chunks.Add(index, chunk);
+                        updatePriority.Enqueue(chunk, new ChunkPriority(0));
                     }
 
-                    chunk.approximateNormals = baseChunk.approximateNormals || ShouldApproximateNormals(dist);
-                    chunk.quality = GetQuality(dist);
-
-                    chunk.GenerateMesh();
+                    newChunks.Add(index, chunk);
                 }
             }
         }
-        Debug.Log("Delta time: " + Time.deltaTime);
+
+        foreach (Int3 index in chunks.Keys)
+        {
+            Destroy(chunks[index].gameObject);
+            updatePriority.Remove(chunks[index]);
+        }
+
+        chunks = newChunks;
+
+        activeViewDistance = dist;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RegenerateChunks(viewDistance);
+
+        for (int i = 0; i < updatesPerTick; i++)
+        {
+            Chunk chunk = updatePriority.Dequeue();
+
+            float dist = chunk.transform.localPosition.magnitude;
+
+            chunk.approximateNormals = baseChunk.approximateNormals || ShouldApproximateNormals(dist);
+            chunk.quality = GetQuality(dist);
+
+            chunk.GenerateMesh();
+
+            updatePriority.Enqueue(chunk, new ChunkPriority(Time.time));
+        }
     }
 }
--- Assets/Scripts/Universe.cs
+++ Assets/Scripts/Universe.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Universe : MonoBehaviour
 {
+    public static readonly double DELTA_RATIO = 0.9;
+
     public static Universe Instance { get; private set; }
 
     public Player mainPlayer;
 
-    private void Awake()
+    public double FPSAim;
+
+    public double TimeDeltaAim { get => 1 / FPSAim; }
+
+    public double TimeDeltaAverage = .1;
+
+    void Awake()
     {
         Instance = this;
+    }
+
+    void Update()
+    {
+        TimeDeltaAverage *= DELTA_RATIO;
+        TimeDeltaAverage += Time.deltaTime * (1 - DELTA_RATIO);
     }
 }
