a51702a9409e0aaf4bdd30e8bd9095614692bd3c
Joeoc2001 <joeoc2625@gmail.com>
2020-10-03 02:23:39 +0100

    Added planar weighting algorithm to quadratic object

--- Assets/Scripts/Rendering/Chunks/DualContouringChunk/DualContouringGenerator.cs
+++ Assets/Scripts/Rendering/Chunks/DualContouringChunk/DualContouringGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 namespace SDFRendering.Chunks.SurfaceNetChunk
@@ -6,6 +6,91 @@ namespace SDFRendering.Chunks.SurfaceNetChunk
     public class DualContouringGenerator : DualGenerator
     {
         public static readonly DualContouringGenerator Instance = new DualContouringGenerator();
+
+        // Has no constant term as constant term can be ignored when finding minimum
+        private class Unbounded3DPolynomial
+        {
+            private readonly float[] _coefficients = new float[9];
+
+            public float XSquared {
+                get => _coefficients[0];
+                private set => _coefficients[0] = value;
+            }
+            public float YSquared
+            {
+                get => _coefficients[1];
+                private set => _coefficients[1] = value;
+            }
+            public float ZSquared
+            {
+                get => _coefficients[2];
+                private set => _coefficients[2] = value;
+            }
+            public float XY
+            {
+                get => _coefficients[3];
+                private set => _coefficients[3] = value;
+            }
+            public float XZ
+            {
+                get => _coefficients[4];
+                private set => _coefficients[4] = value;
+            }
+            public float YZ
+            {
+                get => _coefficients[5];
+                private set => _coefficients[5] = value;
+            }
+            public float X
+            {
+                get => _coefficients[6];
+                private set => _coefficients[6] = value;
+            }
+            public float Y
+            {
+                get => _coefficients[7];
+                private set => _coefficients[7] = value;
+            }
+            public float Z
+            {
+                get => _coefficients[8];
+                private set => _coefficients[8] = value;
+            }
+
+            public static Unbounded3DPolynomial operator +(Unbounded3DPolynomial a, Unbounded3DPolynomial b)
+            {
+                Unbounded3DPolynomial n = new Unbounded3DPolynomial();
+                for (int i = 0; i < n._coefficients.Length; i++)
+                {
+                    n._coefficients[i] = a._coefficients[i] + b._coefficients[i];
+                }
+                return n;
+            }
+
+            public Unbounded3DPolynomial AddPlanarWeighting(Vector3 position, Vector3 normal)
+            {
+                normal = normal.normalized;
+
+                float c = normal.x * position.x + normal.y * position.y + normal.z * position.z;
+                Unbounded3DPolynomial newPoly = new Unbounded3DPolynomial
+                {
+                    XSquared = normal.x * normal.x,
+                    YSquared = normal.y * normal.y,
+                    ZSquared = normal.z * normal.z,
+
+                    XY = 2 * normal.x * normal.y,
+                    XZ = 2 * normal.x * normal.z,
+                    YZ = 2 * normal.y * normal.z,
+
+                    X = -2 * c * normal.x,
+                    Y = -2 * c * normal.y,
+                    Z = -2 * c * normal.z
+                };
+
+
+                return this + newPoly;
+            }
+        }
 
         private DualContouringGenerator() : base(0, 1, 1)
         {
